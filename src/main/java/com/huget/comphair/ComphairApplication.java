7c9c3ff92baf4889eb19e32778d1a5183ccfa917
magdalenahuget <magdalena.huget@gmail.com>
2023-10-08 17:49:35 +0200

    Initial commit

---
+++ src/main/java/com/huget/comphair/ComphairApplication.java
@@ -0,0 +1,13 @@
+package com.huget.comphair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComphairApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComphairApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/huget/comphair/ComphairApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huget.comphair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComphairApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
