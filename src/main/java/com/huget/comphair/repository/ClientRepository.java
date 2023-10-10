338bcc7a61727c25d694a9a4d9e8acf68a2b568e
magdalenahuget <magdalena.huget@gmail.com>
2023-10-10 19:51:37 +0200

    Add client repository implementation

--- src/main/java/com/huget/comphair/repository/ClientRepository.java
+++ src/main/java/com/huget/comphair/repository/ClientRepository.java
@@ -1,4 +1,21 @@
 package com.huget.comphair.repository;
 
-public class ClientRepository {
+import com.huget.comphair.model.Client;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+
+    List<Client> findByHairdresserId(long hairdresserId);
+
+    @Transactional
+    void deleteAllClientsOfHairdresser(long hairdresserId);
+
+    List<Client> findClientsByLastName(String lastName);
+
+    List<Client> findAllClientsByHairdresserName(String hairdresserName);
 }
