69ce41503b77878aea88b0a7e3d717209c0c792c
magdalenahuget <magdalena.huget@gmail.com>
2024-01-05 23:08:37 +0100

    Feature: add hairdresser service methods

--- src/main/java/com/huget/comphair/service/HairdresserImpl.java
+++ src/main/java/com/huget/comphair/service/HairdresserImpl.java
@@ -1,4 +1,37 @@
 package com.huget.comphair.service;
 
-public class HairdresserImpl {
+import com.huget.comphair.model.Hairdresser;
+
+import java.util.List;
+
+public class HairdresserImpl implements HairdresserService{
+    @Override
+    public List<Hairdresser> getAllHairdressers() {
+        return null;
+    }
+
+    @Override
+    public Hairdresser getHairdresser() {
+        return null;
+    }
+
+    @Override
+    public Hairdresser createHairdresser() {
+        return null;
+    }
+
+    @Override
+    public Hairdresser updateHairdresser() {
+        return null;
+    }
+
+    @Override
+    public void deleteHairdresser() {
+
+    }
+
+    @Override
+    public void deleteAllHairdressers() {
+
+    }
 }
