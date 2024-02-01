7b19e7902754b608764601459233b2e914155bf9
magdalenahuget <magdalena.huget@gmail.com>
2024-02-01 23:08:30 +0100

    Feature: add methods to hairdresser details service interface

---
+++ src/main/java/com/huget/comphair/service/HairdresserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package com.huget.comphair.service;
+
+import com.huget.comphair.model.HairdresserDetails;
+
+public class HairdresserDetailsServiceImpl implements HairdresserDetailsService{
+
+
+    @Override
+    public HairdresserDetails getDetailsById(Long id) {
+        return null;
+    }
+
+    @Override
+    public HairdresserDetails createDetails(Long hairdresserId, HairdresserDetails hairdresserDetails) {
+        return null;
+    }
+
+    @Override
+    public HairdresserDetails updateDescription(Long id, HairdresserDetails hairdresserDetails) {
+        return null;
+    }
+
+    @Override
+    public void deleteDetailsById(Long id) {
+
+    }
+
+    @Override
+    public void deleteDetailsOfHairdresser(Long hairdresserId) {
+
+    }
+}
