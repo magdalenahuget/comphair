bcca88cc4d6adc0d70d8872683489f8a6e8445d1
magdalenahuget <magdalena.huget@gmail.com>
2024-02-02 18:01:55 +0100

    Feature: add implementation to action service

--- src/main/java/com/huget/comphair/controller/ActionController.java
+++ src/main/java/com/huget/comphair/controller/ActionController.java
@@ -1,16 +1,14 @@
 package com.huget.comphair.controller;
 
-import com.huget.comphair.exception.ResourceNotFoundException;
 import com.huget.comphair.model.Action;
 import com.huget.comphair.model.Hairdresser;
 import com.huget.comphair.repository.ActionRepository;
 import com.huget.comphair.repository.HairdresserRepository;
+import com.huget.comphair.service.ActionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -19,16 +17,14 @@ import java.util.List;
 public class ActionController {
 
     @Autowired
-    private HairdresserRepository hairdresserRepository;
+    private ActionRepository actionRepository;
 
     @Autowired
-    private ActionRepository actionRepository;
+    private ActionService actionService;
 
     @GetMapping("/actions")
     public ResponseEntity<List<Action>> getAllAction(){
-        List<Action> actions = new ArrayList<Action>();
-
-        actionRepository.findAll().forEach(actions::add);
+        List<Action> actions = actionService.getAllActions();
 
         if(actions.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
@@ -38,58 +34,31 @@ public class ActionController {
 
     @GetMapping("/hairdressers/{hairdresserId}/actions")
     public ResponseEntity<List<Action>> getAllActionsByHairdresserId(@PathVariable(value = "hairdresserId") Long hairdresserId){
-        if(!hairdresserRepository.existsById(hairdresserId)) {
-            throw new ResourceNotFoundException("Not found hairdresser with id = " + hairdresserId);
-        }
-        List<Action> actions = actionRepository.findActionByHairdressersId(hairdresserId);
+        List<Action> actions = actionService.getAllActionsByHairdresserId(hairdresserId);
         return new ResponseEntity<>(actions, HttpStatus.OK);
     }
 
     @GetMapping("/actions/{id}")
     public ResponseEntity<Action> getActionsById(@PathVariable(value="id") Long id){
-        Action action = actionRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Not found action with id = " + id));
-
+        Action action = actionService.getActionById(id);
         return new ResponseEntity<>(action, HttpStatus.OK);
     }
 
     @GetMapping("/actions/{actionId}/hairdressers")
     public ResponseEntity<List<Hairdresser>> getAllHairdressersByActionsId(@PathVariable(value = "actionId") Long actionId) {
-        if (!actionRepository.existsById(actionId)) {
-            throw new ResourceNotFoundException("Not found action with id = " + actionId);
-        }
-
-        List<Hairdresser> hairdressers = hairdresserRepository.findHairdressersByActionsId(actionId);
+        List<Hairdresser> hairdressers = actionService.getAllHairdressersByActionId(actionId);
         return new ResponseEntity<>(hairdressers, HttpStatus.OK);
     }
 
     @PostMapping("/hairdressers/{hairdresserId}/actions")
     public ResponseEntity<Action> addAction(@PathVariable(value = "hairdresserId") Long hairdresserId, @RequestBody Action actionRequest) {
-        Action action = hairdresserRepository.findById(hairdresserId).map(hairdresser -> {
-            long actionId = actionRequest.getId();
-
-            if (actionId != 0L) {
-                Action actionFromRequest = actionRepository.findById(actionId)
-                        .orElseThrow(() -> new ResourceNotFoundException("Not found action with id = " + actionId));
-                hairdresser.addAction(actionFromRequest);
-                hairdresserRepository.save(hairdresser);
-                return actionFromRequest;
-            }
-
-            hairdresser.addAction(actionRequest);
-            return actionRepository.save(actionRequest);
-        }).orElseThrow(() -> new ResourceNotFoundException("Not found hairdresser with id = " + hairdresserId));
-
+        Action action = actionService.addAction(hairdresserId, actionRequest);
         return new ResponseEntity<>(action, HttpStatus.CREATED);
     }
 
     @PutMapping("/actions/{id}")
     public ResponseEntity<Action> updateAction(@PathVariable("id") long id, @RequestBody Action actionRequest) {
-        Action action = actionRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("action id " + id + "not found"));
-
-        action.setName(actionRequest.getName());
-
+        Action action = actionService.updateAction(id, actionRequest);
         return new ResponseEntity<>(actionRepository.save(action), HttpStatus.OK);
     }
 
@@ -97,23 +66,13 @@ public class ActionController {
     public ResponseEntity<HttpStatus> deleteActionFromHairdresser(@PathVariable(value = "hairdresserId")
                                                                       Long hairdresserId, @PathVariable(value = "actionId")
     Long actionId) {
-        Hairdresser hairdresser = hairdresserRepository.findById(hairdresserId)
-                .orElseThrow(() -> new ResourceNotFoundException("Not found hairdresser with id = " + hairdresserId));
-
-        hairdresser.removeAction(actionId);
-        hairdresserRepository.save(hairdresser);
-
+        actionService.deleteActionFromHairdresser(hairdresserId, actionId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @DeleteMapping("/actions/{id}")
     public ResponseEntity<HttpStatus> deleteAction(@PathVariable("id") long id) {
-        List<Hairdresser> hairdressers = hairdresserRepository.findHairdressersByActionsId(id);
-        for (Hairdresser hairdresser : hairdressers) {
-            hairdresser.removeAction(id);
-        }
-        hairdresserRepository.saveAll(hairdressers);
-        actionRepository.deleteById(id);
+        actionService.deleteAction(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
