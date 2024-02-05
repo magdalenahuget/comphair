3f2568aa3906029ee12911338bfb67b2bc5d8389
magdalenahuget <magdalena.huget@gmail.com>
2024-02-05 22:23:51 +0100

    Feature: add create default roles method

--- src/main/java/com/huget/comphair/ComphairApplication.java
+++ src/main/java/com/huget/comphair/ComphairApplication.java
@@ -1,13 +1,21 @@
 package com.huget.comphair;
 
+import com.huget.comphair.repository.RoleRepository;
+import com.huget.comphair.service.RoleService;
+import com.huget.comphair.service.RoleServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ComphairApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ComphairApplication.class, args);
+//        SpringApplication.run(ComphairApplication.class, args);
+
+        ConfigurableApplicationContext context = SpringApplication.run(ComphairApplication.class, args);
+        RoleService roleService = context.getBean(RoleService.class);
+        roleService.createRoles();
     }
 
 }
